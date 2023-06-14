31a512079f3f817e56068b6735498cf814ce65b8
Ramin11307 <ramin.saghari2021@gmail.com>
2023-06-13 23:30:49 -0700

    all project

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:clima/screens/loading_screen.dart';
+import 'package:co.Weather/screens/loading_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,7 +7,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.white(),
+      theme: ThemeData.dark(),
+      
       home: LoadingScreen(),
     );
   }
